0e33084978287bc93fb7ae42384788da45d8356e
will14lee <will14lee@gmail.com>
2021-06-13 02:36:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+function findMatching(array, name){
+    const lowerCaseName= name.toLowerCase()
+      return array.filter(name=> name.toLowerCase()=== lowerCaseName);
+  }
+console.log(findMatching(drivers, "Bobby"))
+
+function fuzzyMatch(array, name){
+    const firstTwoLetters=name.slice(0, 2)
+    return array.filter(name=>name.slice(0, 2)=== firstTwoLetters)
+}
+
+function matchName(array, string){
+    return array.filter((string)=>"Bobby"=== string.name)
+}
